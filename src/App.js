c96b93bd51d83f8c6fa9b595544ea1a4d26e31c1
jd-create <jenniferdemeijer@t-online.de>
2021-05-20 21:36:55 +0200

    add 3 buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import './App.css';
 function App() {
   return (
       <h1>Handbags & Purses</h1>
+      <nav className="nav">
+  <button type="button"
+          onClick={() => console.log("to the collection")
+          }
+  >
+    to the collection
+  </button>
+  <button type="button"
+          onClick={() => console.log("shop all bags")
+          }
+  >
+    shop all bags
+  </button>
+</button>
+<button type="button"
+onClick={() => console.log("pre-orders")
+  }
+>
+  pre-orders
+  </button>
+</nav>
   );
 }
 
